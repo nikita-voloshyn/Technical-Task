5fdad9d053046683d77c849b299c2c281fac7e8f
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2024-07-07 18:30:42 +0300

    +++

--- Part 1: Resume Parsing /robotaua_parser.py
+++ Part 1: Resume Parsing /robotaua_parser.py
@@ -5,11 +5,11 @@ import json
 import re
 from datetime import datetime
 
-# URL для POST-запроса и URL для GET-запроса с параметром markView: true
+# URL
 url_post = "https://employer-api.robota.ua/cvdb/resumes"
 url_get_template = "https://employer-api.robota.ua/resume/{resume_id}?markView=true"
 
-# Параметры для POST-запроса
+
 payload = {
     "page": 1,
     "period": "ThreeMonths",
@@ -40,40 +40,34 @@ payload = {
     "districtIds": [],
     "onlyStudents": False,
     "searchContext": "Filters",
-    "requestedCount": 20  # Добавляем requestedCount
+    "requestedCount": 20  # Додаємо requestedCount
 }
 
-# Заголовки для запроса (если необходимо)
+
 headers = {
     "Content-Type": "application/json",
-    # Добавьте здесь любые другие заголовки, если это необходимо, например, токен авторизации
+#     можна додати ще але воно не блокує тому добре
 }
 
-
-# Функция для обработки POST-запроса с возможностью повтора
 async def send_request(session, url, payload, headers, retries=3):
     for attempt in range(retries):
         async with session.post(url, json=payload, headers=headers) as response:
             if response.status == 200:
                 return await response.json()
             else:
-                print(f"Попытка {attempt + 1} не удалась. Статус код: {response.status}")
+                print(f"Спроба {attempt + 1} не вдалася. Статус код: {response.status}")
                 await asyncio.sleep(2)
     return None
 
-
-# Функция для отправки GET-запроса с использованием aiohttp
 async def fetch_resume(session, resume_id):
     url = url_get_template.format(resume_id=resume_id)
     async with session.get(url) as response:
         if response.status == 200:
             return await response.json()
         else:
-            print(f"Ошибка при получении данных кандидата {resume_id}: {response.status}")
+            print(f"Помилка при отриманні даних кандидата {resume_id}: {response.status}")
             return None
 
-
-# Функция для расчета общего времени работы
 def calculate_total_experience(experiences):
     total_months = 0
     for exp in experiences:
@@ -89,54 +83,43 @@ def calculate_total_experience(experiences):
     months = total_months % 12
     return f"{years} років і {months} місяців"
 
-
-# Функция для получения всех резюме и отправки GET-запросов
 async def get_and_process_resumes():
     async with aiohttp.ClientSession() as session:
         all_responses = []
 
-        # Цикл для пагинации
         while True:
             json_data = await send_request(session, url_post, payload, headers)
 
             if json_data:
-                print(f"Страница {payload['page']} обработана.")
+                print(f"Сторінка {payload['page']} оброблена.")
 
-                # Добавляем данные о людях в список
                 all_responses.extend(json_data.get('documents', []))
 
-                # Проверка количества документов
                 if len(json_data.get('documents', [])) < payload['requestedCount']:
-                    print("Достигнут конец данных.")
+                    print("Досягнуто кінець даних.")
                     break
 
-                # Увеличение номера страницы для следующего запроса
                 payload['page'] += 1
 
-                # Задержка между запросами
                 await asyncio.sleep(2)
             else:
-                print(f"Ошибка при выполнении запроса на странице {payload['page']}")
+                print(f"Помилка при виконанні запиту на сторінці {payload['page']}")
                 break
 
-        # Получение дополнительных данных о каждом кандидате
         extracted_data = []
         tasks = [fetch_resume(session, resume['resumeId']) for resume in all_responses]
         results = await asyncio.gather(*tasks)
 
         for details in results:
             if details:
-                # Очистка описания навыков от HTML тегов и получение текста
                 cleaned_skills = []
                 for skill in details.get('skills', []):
                     description = skill.get('description', '')
-                    clean_description = re.sub(r"<.*?>", "", description)  # Удаление всех HTML тегов
+                    clean_description = re.sub(r"<.*?>", "", description)  # Видалення всіх HTML тегів
                     cleaned_skills.append(clean_description)
 
-                # Расчет общего времени работы
                 total_experience = calculate_total_experience(details.get('experiences', []))
 
-                # Добавление данных о кандидате в извлеченные данные
                 data = {
                     'resume_id': details.get('resumeId', 'Not specified'),
                     'name': details.get('name', 'Not specified'),
@@ -148,13 +131,10 @@ async def get_and_process_resumes():
                 }
                 extracted_data.append(data)
 
-        # Сохранение извлеченных данных в файл
         with open('robotaua_data.json', 'w', encoding='utf-8') as jsonfile:
             json.dump(extracted_data, jsonfile, ensure_ascii=False, indent=4)
 
-        print("Извлеченные данные сохранены в файл extracted_data.json.")
+        print("Отримані дані збережено у файл robotaua_data.json.")
 
-
-# Запуск основной функции
 if __name__ == "__main__":
     asyncio.run(get_and_process_resumes())
--- Part 1: Resume Parsing /workua_parser.py
+++ Part 1: Resume Parsing /workua_parser.py
@@ -6,31 +6,16 @@ import re
 
 class WorkUaScraper:
     def __init__(self, base_url, start_page=1, end_page=10):
-        """
-        Инициализация скрапера с базовым URL и диапазоном страниц.
 
-        Аргументы:
-        base_url (str): Базовый URL для страниц с резюме.
-        start_page (int): Начальная страница для парсинга.
-        end_page (int): Конечная страница для парсинга.
-        """
         self.base_url = base_url
         self.start_page = start_page
         self.end_page = end_page
 
     def get_links_from_page(self, url):
-        """
-        Извлечение ссылок с одной страницы.
 
-        Аргументы:
-        url (str): URL страницы.
-
-        Возвращает:
-        list: Список ссылок на резюме.
-        """
         try:
             response = requests.get(url)
-            response.raise_for_status()  # Проверка успешности запроса
+            response.raise_for_status()
         except requests.RequestException as e:
             print(f"Error fetching {url}: {e}")
             return []
@@ -40,28 +25,12 @@ class WorkUaScraper:
         return links
 
     def extract_numbers(self, text):
-        """
-        Извлечение чисел из текста.
 
-        Аргументы:
-        text (str): Входной текст.
-
-        Возвращает:
-        str: Строка, содержащая только числа.
-        """
         numbers = re.findall(r'\d+', text)
         return ' '.join(numbers) if numbers else "Not specified"
 
     def extract_position_and_salary(self, tag):
-        """
-        Извлечение позиции и зарплаты из тега.
 
-        Аргументы:
-        tag (bs4.element.Tag): Тег, содержащий позицию и зарплату.
-
-        Возвращает:
-        tuple: Позиция и зарплата.
-        """
         if tag:
             full_text = tag.get_text(separator=" ", strip=True)
             salary_tag = tag.find("span", class_="text-muted-print")
@@ -83,15 +52,7 @@ class WorkUaScraper:
         return position, salary
 
     def extract_experience(self, text):
-        """
-        Извлечение опыта работы из текста.
 
-        Аргументы:
-        text (str): Текст с опытом работы.
-
-        Возвращает:
-        str: Строка с опытом работы.
-        """
         experience = "Not specified"
         match = re.search(r'\((.*?)\)', text)
         if match:
@@ -99,44 +60,31 @@ class WorkUaScraper:
         return experience
 
     def scrape_resume_data(self, resume_url):
-        """
-        Сбор данных с конкретного резюме, включая опыт работы.
 
-        Аргументы:
-        resume_url (str): URL резюме.
-
-        Возвращает:
-        dict: Собранные данные с резюме.
-        """
         try:
             response = requests.get(resume_url)
-            response.raise_for_status()  # Проверка успешности запроса
+            response.raise_for_status()
         except requests.RequestException as e:
             print(f"Error fetching {resume_url}: {e}")
             return {}
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Получаем ID резюме из URL
         resume_id = resume_url.replace("https://www.work.ua/resumes/", "").replace("/", "")
 
-        # Формируем CSS-селекторы
         name_selector = f'#resume_{resume_id} > div:nth-of-type(1) > div > div > h1'
         position_selector = f'#resume_{resume_id} > div:nth-of-type(1) > div > div > h2'
         dl_selector = f'#resume_{resume_id} > div:nth-of-type(1) > div > div > dl'
-        skill_selector = f'li.no-style.mr-sm.mt-sm'  # CSS-селектор для элемента с классом "no-style mr-sm mt-sm"
-        experience_section_selector = f'#resume_{resume_id} > h2:nth-of-type(2)'  # XPath для заголовка "Досвід роботи"
-        experience_selector = f'#resume_{resume_id} > p:nth-of-type(3) > span:nth-of-type(1)'  # XPath для опыта работы
+        skill_selector = f'li.no-style.mr-sm.mt-sm'
+        experience_section_selector = f'#resume_{resume_id} > h2:nth-of-type(2)'
+        experience_selector = f'#resume_{resume_id} > p:nth-of-type(3) > span:nth-of-type(1)'
 
-        # Извлекаем данные о позиции и зарплате
         job_position_tag = soup.select_one(position_selector)
         job_position, salary_expectation = self.extract_position_and_salary(job_position_tag)
 
-        # Извлекаем имя
         name_tag = soup.select_one(name_selector)
         name = name_tag.get_text(strip=True) if name_tag else "Not specified"
 
-        # Извлекаем данные из dl-тега
         dl_tag = soup.select_one(dl_selector)
         location = age = willingness_to_work = "Not specified"
         if dl_tag:
@@ -149,13 +97,11 @@ class WorkUaScraper:
                 elif "Готовий працювати" in dt.get_text():
                     willingness_to_work = dt.find_next_sibling("dd").get_text(strip=True)
 
-        # Извлекаем тексты из элементов с классом "no-style mr-sm mt-sm"
         skill_texts = []
         skill_tags = soup.select(skill_selector)
         for tag in skill_tags:
             skill_texts.append(tag.get_text(strip=True))
 
-        # Проверяем наличие "Досвід роботи" перед извлечением опыта работы
         experience_section_tag = soup.select_one(experience_section_selector)
         if experience_section_tag and "Досвід роботи" in experience_section_tag.get_text(strip=True):
             experience_tag = soup.select_one(experience_selector)
@@ -164,10 +110,8 @@ class WorkUaScraper:
         else:
             experience = "Not specified"
 
-        # Форматирование навыков в строку
         skills = "• ".join(skill_texts)
 
-        # Добавляем скилы в данные резюме
         resume_data = {
             'resume_id': resume_id,
             'name': name,
@@ -181,12 +125,7 @@ class WorkUaScraper:
         return resume_data
 
     def scrape_all_resumes(self):
-        """
-        Сбор данных со всех резюме.
 
-        Возвращает:
-        list: Список данных со всех резюме.
-        """
         all_links = []
         for page in range(self.start_page, self.end_page + 1):
             url = f"{self.base_url}{page}"
@@ -202,31 +141,17 @@ class WorkUaScraper:
         return all_resume_data
 
     def save_to_json(self, data, filename="workua_data.json"):
-        """
-        Сохранение собранных данных резюме в JSON файл.
 
-        Аргументы:
-        data (list): Список словарей с данными резюме.
-        filename (str): Название файла JSON для сохранения. По умолчанию 'resume_data.json'.
-        """
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-# Пример использования
 if __name__ == "__main__":
-    # Укажите базовый URL и диапазон страниц для парсинга
     base_url = "https://www.work.ua/resumes-web+developer/?page="
     start_page = 1
-    end_page = 5  # Измените это значение в зависимости от количества страниц, которые хотите спарсить
+    end_page = 48
 
-    # Создание экземпляра класса и запуск парсера
     scraper = WorkUaScraper(base_url, start_page, end_page)
     resume_data = scraper.scrape_all_resumes()
 
-    # Сохранение данных в JSON файл
-    scraper.save_to_json(resume_data, filename="resume_data.json")
-
-    # Вывод результатов (опционально)
-    for data in resume_data:
-        print(data)
+    scraper.save_to_json(resume_data, filename="workua_data.json")
